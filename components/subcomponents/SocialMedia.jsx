f0a73269b32d69f354c9304eeba4f8ac699fdf89
Mulumia Baraka Mshindi <barakamulumia@gmail.com>
2022-05-24 20:30:40 +0300

    Create SocialMedia.jsx

---
+++ components/subcomponents/SocialMedia.jsx
@@ -0,0 +1,24 @@
+import { IconButton, Stack, useColorModeValue } from "@chakra-ui/react";
+
+import ExternalLink from "./ExternalLink";
+import { SocialMediaLinks } from "../../lib/constants";
+
+const SocialMediaIcon = ({ link, name, Icon }) => {
+    const color = useColorModeValue("#330C2F", "#FBFBFF");
+    return (
+        <ExternalLink to={link}>
+            <Icon size={32} fill={color} />
+        </ExternalLink>
+    );
+};
+
+const SocialMedia = () => {
+    return (
+        <Stack direction={"row"} spacing={{ base: 4, sm: 8 }}>
+            {SocialMediaLinks.map((link) => (
+                <SocialMediaIcon {...link} key={link.name} />
+            ))}
+        </Stack>
+    );
+};
+export default SocialMedia;
