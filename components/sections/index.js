2c1272eaf060cf5f73f8603dc52f17a397f1220f
Mulumia Baraka Mshindi <barakamulumia@gmail.com>
2022-05-24 18:45:34 +0300

    feat: :construction: Added about me section

--- components/sections/index.js
+++ components/sections/index.js
@@ -1,1 +1,5 @@
 export { default as HeroSection } from "./HeroSection";
+export { default as AboutSection } from "./AboutSection";
+export { default as ProjectsSection } from "./ProjectsSection";
+
+export { default as ContactSection } from "./ContactSection";
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
+import { AboutSection, ContactSection, HeroSection, ProjectsSection } from "../components/sections";
 import { Box, Flex, Text, VStack } from "@chakra-ui/react";
 
-import { HeroSection } from "../components/sections";
+import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 
 export default function Home() {
@@ -12,7 +13,17 @@ export default function Home() {
                     <SectionWrapper mt={"80px"}>
                         <HeroSection />
                     </SectionWrapper>
+                    <SectionWrapper align={"start"}>
+                        <AboutSection />
+                    </SectionWrapper>
+                    {/* <SectionWrapper align={"start"}>
+                        <ProjectsSection />
+                    </SectionWrapper>
+                    <SectionWrapper align={"start"}>
+                        <ContactSection />
+                    </SectionWrapper> */}
                 </VStack>
+                <Footer />
             </Box>
         </Box>
     );
@@ -21,7 +32,9 @@ export default function Home() {
 const SectionWrapper = ({ children, ...props }) => {
     return (
         <Flex justify={"center"} align={"center"} minHeight={"calc(100vh - 100px)"} {...props}>
-            {children}
+            <Box maxW={"1280px"} minW={"364px"} py={8} px={4} flexGrow={1}>
+                {children}
+            </Box>
         </Flex>
     );
 };
