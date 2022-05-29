acc689d9ce13d3bbe8d1f9b4619c620b14b2f7fe
Mulumia Baraka Mshindi <barakamulumia@gmail.com>
2022-05-29 13:08:02 +0300

    refactor: :art: Refactor theme toggle icon to its own file

---
+++ components/subcomponents/ThemeToggleIcon.jsx
@@ -0,0 +1,20 @@
+import { IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react";
+
+import { FaMoon } from "react-icons/fa";
+import { MdOutlineLightMode } from "react-icons/md";
+
+const ThemeToggleIcon = () => {
+    const { toggleColorMode, colorMode } = useColorMode();
+    const themeIconColor = useColorModeValue("primary.dark.200", "primary.light.200");
+
+    return (
+        <IconButton
+            color={themeIconColor}
+            aria-label={"Change theme"}
+            variant={"transparent"}
+            icon={colorMode === "light" ? <FaMoon size={24} /> : <MdOutlineLightMode size={24} />}
+            onClick={toggleColorMode}
+        />
+    );
+};
+export default ThemeToggleIcon;
