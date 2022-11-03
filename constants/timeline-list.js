fad2e6b48b2d2b8af22b6be20025062a19ddb9a7
Baraka M Mulumia <65751779+BarakaMulumia@users.noreply.github.com>
2022-11-03 23:54:59 +0300

    Create timeline-list.js

---
+++ constants/timeline-list.js
@@ -0,0 +1,35 @@
+export const timelineItemList = [
+    {
+        date: "August 2022",
+        role: "Full Stack Software Engineer",
+        company: "Iworker.co (Remote)",
+        companyLink: "https://iworker.co/",
+        description:
+            "Pioneering the future of work by automating day-to-day tasks and building software platforms for businesses to market, hire and manage their remote workforce.",
+    },
+
+    {
+        date: "March 2022",
+        role: "Front End Developer",
+        company: "Edvora INC (Remote)",
+        companyLink: "https://edvora.com/",
+        description:
+            "Worked with a team of  engineers and spearheaded the management and development of multiple complex modules of the Edvora Next Generation Edu-Tech platform. I was responsible for the development of the front-end of the platform using Agora, NextJs, Redux, and Chakra UI.",
+    },
+    {
+        date: "Jan 2021",
+        role: "Software Development Trainer",
+        company: "Institute of Software Technologies",
+        companyLink: "https://www.isteducation.com/",
+        description:
+            "Developed and delivered both in-person and online training to 100+ students in the areas of Full Stack Web Development With JavaScript, React, Node, Express, MongoDB and  TypeScript.",
+    },
+    {
+        date: "Sept 2020",
+        role: "FreeLance Web Developer",
+        company: "Freelance",
+        companyLink: "https://www.freelancer.com/",
+        description:
+            "Worked with a Hotel management  client on a web application using NextJs, Chakra-UI, NodeJs and MongoDB. The application was used to manage the hotel's inventory, bookings, and payments.",
+    },
+];
