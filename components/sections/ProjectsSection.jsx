0df4860b198d05572d7eb029892c6e3a8d1348b8
Baraka M Mulumia <65751779+BarakaMulumia@users.noreply.github.com>
2022-11-03 23:56:19 +0300

    Update ProjectsSection.jsx

--- components/sections/ProjectsSection.jsx
+++ components/sections/ProjectsSection.jsx
@@ -4,31 +4,9 @@ import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 
 import { A11y } from "swiper";
 import ProjectCard from "../ProjectCard";
+import React from "react";
+import { projects } from "../../constants/projects-list";
 import { randInt } from "../../lib/functions";
-
-const projects = [
-    {
-        id: "P100",
-        name: "Beyond the edge",
-        desc: "A Coding campaign blog that aims at inspiring and sharing developer stories, To keep your motivation up, think of all the things you've always wanted to create",
-        technologies: ["NextJs", "Sanity-Studio"],
-        img: "/image-assets/projects/baraka-mulumia-blog.png",
-    },
-    {
-        id: "P101",
-        name: "Tender Bureau",
-        desc: "A single information platform to get real-time database for contractors and suppliers looking for government and private tendering business opportunities",
-        technologies: ["NextJS", "Sanity-Studio", "MongoDB"],
-        img: "/image-assets/projects/tender-bureau-tenders-image.png",
-    },
-    {
-        id: "P102",
-        name: "Translify",
-        desc: "Lead Generation Specialists for Moving Products and Goods, A freight forwarding platform to make commodities be where and when they are demanded by the you.",
-        technologies: ["ReactJs", "NodeJs", "MongoDB"],
-        img: "/image-assets/projects/translify-landing-page.png",
-    },
-];
 
 const SlideNextButton = () => {
     const swiper = useSwiper();
@@ -46,6 +24,7 @@ const SlideNextButton = () => {
 const SlidePrevButton = () => {
     const swiper = useSwiper();
     const fillColor = useColorModeValue("primary.dark.100", "primary.light.100");
+    const buttonBg = useColorModeValue("primary.light.300", "primary.dark.300");
 
     return (
         <IconButton
@@ -68,13 +47,16 @@ const ProjectsSection = () => {
                 gap={5}
                 py={4}
             >
-                <Text fontSize={"4xl"} fontWeight={600}>
-                    Recent Works
+                <Text fontSize={"3xl"} fontWeight={600}>
+                    Projects
                 </Text>
                 <Text maxW={"600px"} size={"lg"} textAlign={"center"} variant={"textPrimary"}>
                     Learning to code has changed my life because I am now headed down a path to
                     potential economic stability and personal growth. I look forward to working on
-                    complex projects and on things that improve people&apos;s lives.
+                    complex projects and on things that improve people&apos;s lives. <br />
+                    <Text as={"span"} color={"blue.300"}>
+                        Here is a collection of my most interesting projects.
+                    </Text>
                 </Text>
             </Flex>
             <Box display={{ base: "none", mxd: "block" }}>
