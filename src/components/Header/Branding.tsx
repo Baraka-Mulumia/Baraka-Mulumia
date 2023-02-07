3176c27a6fa467442b9e586f6b00e328378747bd
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-07 16:55:41 +0300

    Create Branding.tsx
    closes #1

---
+++ src/components/Header/Branding.tsx
@@ -0,0 +1,22 @@
+import { Box, Text } from "@chakra-ui/react";
+
+import { AnimatedBottomBorder } from "../AnimatedBottomBorder";
+import { FunctionComponent } from "react";
+import Link from "next/link";
+
+export const Branding: FunctionComponent = () => {
+  return (
+    <Link href={"/"}>
+      <AnimatedBottomBorder>
+        <Text
+          as={"h1"}
+          fontFamily={"heading"}
+          fontWeight={"bold"}
+          fontSize={"2xl"}
+        >
+          Baraka M. Mulumia
+        </Text>
+      </AnimatedBottomBorder>
+    </Link>
+  );
+};
