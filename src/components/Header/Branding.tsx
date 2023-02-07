22def1d6be0b4689e25b246310fbf00a3e8a8981
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-07 21:57:40 +0300

    Update Branding.tsx

--- src/components/Header/Branding.tsx
+++ src/components/Header/Branding.tsx
@@ -1,21 +1,27 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Text, useColorModeValue } from "@chakra-ui/react";
 
 import { AnimatedBottomBorder } from "../AnimatedBottomBorder";
 import { FunctionComponent } from "react";
 import Link from "next/link";
 
 export const Branding: FunctionComponent = () => {
+  const headingColor = useColorModeValue(
+    "text.light.headings",
+    "text.dark.headings"
+  );
+
   return (
     <Link href={"/"}>
       <AnimatedBottomBorder>
         <Text
           as={"h1"}
           fontFamily={"heading"}
-          fontWeight={"bold"}
+          fontWeight={700}
           fontSize={{
             base: "xl",
             md: "2xl",
           }}
+          color={headingColor}
         >
           Baraka M. Mulumia
         </Text>
