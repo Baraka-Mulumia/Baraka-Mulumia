927b80ef71557aaa70afa139102fe49c6c7b09f0
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-08 20:58:21 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import AboutSection from "@/sections/About";
-import Banner from "@/sections/Banner";
+import AboutSection from "@/sections/landing-page/About";
+import Banner from "@/sections/landing-page/Banner";
+import { Blog } from "@/sections/landing-page/blog";
 import { Box } from "@chakra-ui/react";
 import { Header } from "@/components/Header/Header";
 import { PageCOntentContainer } from "@/components/layout/PageCOntentContainer";
+import { Projects } from "@/sections/landing-page/Projects";
+import { Services } from "@/sections/landing-page/Services";
 
 export default function Home() {
   return (
@@ -10,6 +13,9 @@ export default function Home() {
       <PageCOntentContainer Nav={Header}>
         <Banner />
         <AboutSection />
+        <Services />
+        <Projects />
+        <Blog />
       </PageCOntentContainer>
     </Box>
   );
