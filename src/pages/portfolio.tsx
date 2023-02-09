06cacabd9527f70cff5d822bb385441026189fc5
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-09 14:14:54 +0300

    Update portfolio.tsx - fix typo

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,9 +1,15 @@
 import { NextPage } from "next";
+import { Text } from "@chakra-ui/react";
 
 const Portfolio: NextPage = () => {
   return (
     <div>
       <h1>Portfolio</h1>
+      <Text>
+        {
+          " As a software developer, I understand the importance of bridging the gap between a client's vision and my expertise to achieve powerful results. This is why I am proud to present my personal portfolio, where I can showcase the projects I've worked on and the impact I've made."
+        }
+      </Text>
     </div>
   );
 };
