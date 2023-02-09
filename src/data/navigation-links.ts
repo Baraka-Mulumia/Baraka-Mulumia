bae0db7e243a890df89f598bc0d1d32d410e92ee
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-09 14:04:23 +0300

    Create navigation-links.ts

---
+++ src/data/navigation-links.ts
@@ -0,0 +1,18 @@
+export const navigationLinks = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "My Story",
+    href: "/blog/my-story",
+  },
+  {
+    name: "Portfolio",
+    href: "/portfolio",
+  },
+  {
+    name: "Get in Touch",
+    href: "/get-in-touch",
+  },
+];
