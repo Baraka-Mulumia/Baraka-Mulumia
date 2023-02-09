7ba8fd94736ae4a1dcb428698cd3b58dd762d0c7
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-09 20:16:46 +0300

    Add services schema closes #15

--- bm-studio/schemas/index.ts
+++ bm-studio/schemas/index.ts
@@ -1,6 +1,7 @@
+import author from './author'
 import blockContent from './blockContent'
 import category from './category'
 import post from './post'
-import author from './author'
+import service from './service'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [service, post, author, category, blockContent]
