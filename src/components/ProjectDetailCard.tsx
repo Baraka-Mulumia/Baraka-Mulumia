ee1b7f30786f5333a1e507ab76a691237e597a77
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-13 23:54:42 +0300

    Create ProjectDetailCard.tsx

---
+++ src/components/ProjectDetailCard.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+import { Box } from "@chakra-ui/react";
+
+type ProjectDetailCardProps = {};
+
+export const ProjectDetailCard: FunctionComponent<
+  ProjectDetailCardProps
+> = ({}) => {
+  return <Box>ajajaj</Box>;
+};
