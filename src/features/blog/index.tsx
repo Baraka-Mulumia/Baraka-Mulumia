4b90e2d94aca57c1edfa0c9224fce5f5951f4ff6
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-13 23:06:30 +0300

    create blog homepage - closes #12

---
+++ src/features/blog/index.tsx
@@ -0,0 +1,43 @@
+import { Box, Grid, GridItem, Stack } from "@chakra-ui/react";
+
+import { BlockContainer } from "@/components/layout/BlockContainer";
+import { BlogPostCard } from "@/components/BlogPostCard";
+import { FunctionComponent } from "react";
+import { PostContent } from "@/lib/types";
+import { map } from "lodash";
+
+export const BlogPosts: FunctionComponent<{
+  posts: PostContent[];
+}> = ({ posts }) => {
+  const firstBlogPost = posts[0];
+
+  // const middleBlogPosts = posts.slice(1, posts.length - 1);
+
+  // const lastBlogPost = posts[posts.length - 1];
+
+  return (
+    <Box as="section" id="blog" py="20" w={"full"}>
+      <BlockContainer w={"full"}>
+        <Stack spacing={4}>
+          <BlogPostCard size={"large"} {...firstBlogPost} />
+
+          {/* <Grid
+            templateColumns={{
+              base: "repeat(1, 1fr)",
+              md: "repeat(2, 1fr)",
+              lg: "repeat(3, 1fr)",
+            }}
+          >
+            {map(middleBlogPosts, (post, index) => (
+              <GridItem key={index}>
+                <BlogCard size={"small"} {...post} />
+              </GridItem>
+            ))}
+          </Grid> */}
+          {/* 
+          <BlogCard size={"large"} isReverse={true} {...lastBlogPost} /> */}
+        </Stack>
+      </BlockContainer>
+    </Box>
+  );
+};
