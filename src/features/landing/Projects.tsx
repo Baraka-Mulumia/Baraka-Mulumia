3e0fdab18803ec791e1367eb3e86f1d0ae42e843
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-13 23:55:25 +0300

    Update portfolio.tsx

--- src/features/landing/Projects.tsx
+++ src/features/landing/Projects.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, HStack } from "@chakra-ui/react";
+import { Box, Button, HStack, StackProps } from "@chakra-ui/react";
 
 import { BlockContainer } from "@/components/layout/BlockContainer";
 import { ChevronRightIcon } from "@chakra-ui/icons";
@@ -9,6 +9,30 @@ import { ProjectPreviewCard } from "@/components/ProjectPreviewCard";
 import { SectionHeader } from "@/components/SectionHeader";
 import { map } from "lodash";
 
+type ProjectListProps = {
+  data: ProjectContent[];
+} & StackProps;
+
+export const ProjectList: FunctionComponent<ProjectListProps> = ({
+  data: projects,
+  ...props
+}) => {
+  return (
+    <HStack
+      w={"full"}
+      spacing={4}
+      mt={8}
+      justifyContent={"center"}
+      flexWrap={"wrap"}
+      {...props}
+    >
+      {map(projects, (project) => (
+        <ProjectPreviewCard key={project.slug.current} project={project} />
+      ))}
+    </HStack>
+  );
+};
+
 export const Projects: FunctionComponent<{
   data: ProjectContent[];
 }> = ({ data: projects }) => {
@@ -17,17 +41,7 @@ export const Projects: FunctionComponent<{
       <BlockContainer w={"full"} minH={"auto"}>
         <SectionHeader title="Some of my work" slogan="The art of creating" />
 
-        <HStack
-          w={"full"}
-          spacing={4}
-          mt={8}
-          justifyContent={"center"}
-          flexWrap={"wrap"}
-        >
-          {map(projects, (project) => (
-            <ProjectPreviewCard key={project.slug.current} project={project} />
-          ))}
-        </HStack>
+        <ProjectList data={projects} />
 
         <HStack spacing={4} mt={8} justifyContent={"flex-end"}>
           <Link href="/portfolio">
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,16 +1,63 @@
+import { Contact } from "@/features/landing/Contact";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header/Header";
 import { NextPage } from "next";
+import { PageCOntentContainer } from "@/components/layout/PageCOntentContainer";
+import { ProjectContent } from "@/lib/types";
+import { ProjectList } from "@/features/landing/Projects";
+import { SanityClient } from "@/lib/sanityClient";
+import { SimpleBanner } from "@/components/SimpleBanner";
 import { Text } from "@chakra-ui/react";
 
-const Portfolio: NextPage = () => {
+const projectsQuery = `*[_type == "project"] | order(_createdAt desc) {
+ title,
+  description,
+  mainImage {
+    asset->{
+      _id,
+      url
+    },
+    alt
+  },
+  slug,
+  url,
+  sourceCodeUrl,
+  isHosted,
+}`;
+
+export async function getStaticProps() {
+  try {
+    const data = await SanityClient.fetch(projectsQuery);
+    console.log(data);
+    return {
+      props: {
+        projects: data,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        projects: [],
+      },
+    };
+  }
+}
+
+const Portfolio: NextPage<{
+  projects: ProjectContent[];
+}> = ({ projects }) => {
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <Text>
-        {
-          " As a software developer, I understand the importance of bridging the gap between a client's vision and my expertise to achieve powerful results. This is why I am proud to present my personal portfolio, where I can showcase the projects I've worked on and the impact I've made."
+    <PageCOntentContainer Nav={Header}>
+      <SimpleBanner
+        title="Art of Code"
+        description={
+          "As a software developer, I understand the importance of bridging the gap between a client's vision and my expertise to achieve powerful results. This is why I am proud to present my personal portfolio, where I can showcase the projects I've worked on and the impact I've made"
         }
-      </Text>
-    </div>
+      />
+      <ProjectList data={projects} spacing={8} />
+      <Contact />
+      <Footer />
+    </PageCOntentContainer>
   );
 };
 
