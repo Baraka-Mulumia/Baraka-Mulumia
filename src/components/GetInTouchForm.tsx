a4a9a9ae823934d1a383254f7f6d65e8c157ef49
Baraka Mshindi Mulumia <65751779+iambaraka@users.noreply.github.com>
2023-02-14 13:43:02 +0300

    Create GetInTouchForm.tsx

---
+++ src/components/GetInTouchForm.tsx
@@ -0,0 +1,61 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Textarea,
+  VStack,
+  WrapItem,
+} from "@chakra-ui/react";
+
+import { BsPerson } from "react-icons/bs";
+import { MdOutlineEmail } from "react-icons/md";
+
+export const GetInTouchForm = () => {
+  return (
+    <WrapItem>
+      <Box bg="white" borderRadius="lg">
+        <Box m={8} color="#0B0E3F">
+          <VStack spacing={5}>
+            <FormControl id="name">
+              <FormLabel>Your Name</FormLabel>
+              <InputGroup borderColor="#E0E1E7">
+                <InputLeftElement
+                  pointerEvents="none"
+                  children={<BsPerson color="gray.800" />}
+                />
+                <Input type="text" size="md" />
+              </InputGroup>
+            </FormControl>
+            <FormControl id="name">
+              <FormLabel>Mail</FormLabel>
+              <InputGroup borderColor="#E0E1E7">
+                <InputLeftElement
+                  pointerEvents="none"
+                  children={<MdOutlineEmail color="gray.800" />}
+                />
+                <Input type="text" size="md" />
+              </InputGroup>
+            </FormControl>
+            <FormControl id="name">
+              <FormLabel>Message</FormLabel>
+              <Textarea
+                borderColor="gray.300"
+                _hover={{
+                  borderRadius: "gray.300",
+                }}
+                placeholder="message"
+              />
+            </FormControl>
+            <FormControl id="name" float="right">
+              <Button variant="primary">Send Message</Button>
+            </FormControl>
+          </VStack>
+        </Box>
+      </Box>
+    </WrapItem>
+  );
+};
