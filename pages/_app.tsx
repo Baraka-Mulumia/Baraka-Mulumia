aa21044825ce73bde6241f730a7c7b924daf6242
Baraka Mulumia <65751779+Baraka-Mulumia@users.noreply.github.com>
2023-12-03 21:56:05 +0300

    fix: :bug: use _app.js for analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import theme, { appFont } from '@/config/theme';
 
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { NotificationProvider } from '@/providers/Notification.provider';
@@ -7,13 +8,16 @@ import SEO from '@/components/SEO';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <SEO />
-      <NotificationProvider>
-        <main className={appFont.className}>
-          <Component {...pageProps} />
-        </main>
-      </NotificationProvider>
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <SEO />
+        <NotificationProvider>
+          <main className={appFont.className}>
+            <Component {...pageProps} />
+          </main>
+        </NotificationProvider>
+      </ChakraProvider>
+      <Analytics />
+    </>
   );
 }
